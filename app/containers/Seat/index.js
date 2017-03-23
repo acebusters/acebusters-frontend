0ea3316d216fecae7a52aaa8739a35fa4f83bf6e
Johann Barbie <johannbarbie@me.com>
2017-03-23 14:24:07 +0100

    remove localstorage for hole cards

--- app/containers/Seat/index.js
+++ app/containers/Seat/index.js
@@ -4,8 +4,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import * as LocalStorage from '../../services/localStorage';
-import { setCards } from '../Table/actions';
 import {
   makeCardSelector,
   makeLastAmountSelector,
@@ -32,20 +30,6 @@ class Seat extends React.PureComponent { // eslint-disable-line react/prefer-sta
   componentWillReceiveProps(nextProps) {
     // Show Action;
     this.opacity = (nextProps.lastAmount !== this.props.lastAmount) ? '1' : 0;
-    // Saving holecards for the hand
-    if (nextProps.hand && nextProps.hand.lineup
-            && nextProps.hand.lineup[this.props.pos]
-            && nextProps.hand.lineup[this.props.pos].last // this will not exist in handComplete, so cards will not be saved by accident
-            && nextProps.hand.lineup[this.props.pos].cards) {
-      const key = `${this.props.params.id}-${this.props.hand.handId}-${this.props.pos}`;
-      LocalStorage.setItem(key, nextProps.hand.lineup[this.props.pos].cards);
-    }
-
-    if (nextProps.hand && nextProps.hand.lineup && nextProps.hand.lineup[this.props.pos] && !nextProps.hand.lineup[this.props.pos].cards && !this.cards) {
-      const key = `${this.props.params.tableAddr}-${this.props.params.handId}-${this.props.pos}`;
-      this.cards = LocalStorage.getItem(key);
-      if (this.cards && this.cards.length === 2) { this.props.setCards(this.props.tableAddr, this.props.params.handId, this.cards, this.props.pos); }
-    }
 
     // manage timer
     if (nextProps.whosTurn === nextProps.pos) {
@@ -74,7 +58,6 @@ class Seat extends React.PureComponent { // eslint-disable-line react/prefer-sta
     // manage timer
     if (this.interval) {
       clearInterval(this.interval);
-      this.interval = null;
     }
   }
 
@@ -101,18 +84,11 @@ const mapStateToProps = createStructuredSelector({
 
 
 export function mapDispatchToProps(dispatch) {
-  return {
-    setCards: (tableAddr, handId, cards, pos) => dispatch(setCards(tableAddr, handId, cards, pos)),
-  };
+  return { dispatch };
 }
 
 Seat.propTypes = {
-  pos: React.PropTypes.number,
-  tableAddr: React.PropTypes.string,
   lastAmount: React.PropTypes.number,
-  hand: React.PropTypes.object,
-  params: React.PropTypes.object,
-  setCards: React.PropTypes.func,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Seat);
