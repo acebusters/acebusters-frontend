81146cb758d47f40249685233b3f898f2d9ac145
Helge Wieding <helge@wieding.com>
2017-03-25 19:17:22 +0100

    fix for table header

--- app/components/Table/TableHeader.js
+++ app/components/Table/TableHeader.js
@@ -3,12 +3,8 @@
  */
 import styled from 'styled-components';
 
-//    margin-top: 10em;
 export const TableHeader = styled.div`
-   @media (max-width: 700px) {
-      display: none;
-   }
    position: relative;
    text-align: left;
-   margin-left: 1em;
+   margin-left: 10em;
 `;
