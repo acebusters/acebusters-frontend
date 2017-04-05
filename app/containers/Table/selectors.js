b3280bc506cd7abf5a82707dd5b182fde84b3f47
Johann Barbie <johannbarbie@me.com>
2017-04-05 19:25:59 +0200

    remove console.log

--- app/containers/Table/selectors.js
+++ app/containers/Table/selectors.js
@@ -116,8 +116,6 @@ const isShowTurnByAction = createSelector(
       return false;
     }
     const whosTurn = pokerHelper.whosTurn(action.hand);
-    console.log(whosTurn);
-    console.log(myPos);
     if (typeof whosTurn === 'undefined' || whosTurn < 0) {
       return false;
     }
