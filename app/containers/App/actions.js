216951eb85727ba91d850d28b351356f13ac838e
KDepp <kdepp.cd@gmail.com>
2017-04-27 22:15:51 +0800

    refactor ProgressBar

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -1,6 +1,7 @@
 export const DEFAULT_LOCALE = 'en';
 export const MODAL_ADD = 'acebusters/App/MODAL_ADD';
 export const MODAL_DISMISS = 'acebusters/App/MODAL_DISMISS';
+export const GLOBAL_PROGRESS = 'acebusters/App/GLOBAL_PROGRESS';
 
 export function modalAdd(node) {
   return { type: MODAL_ADD, node };
@@ -8,4 +9,8 @@ export function modalAdd(node) {
 
 export function modalDismiss() {
   return { type: MODAL_DISMISS };
+}
+
+export function setProgress(progress) {
+  return { type: GLOBAL_PROGRESS, data: progress };
 }
--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -3,11 +3,13 @@ import { fromJS } from 'immutable';
 import {
   MODAL_ADD,
   MODAL_DISMISS,
+  GLOBAL_PROGRESS,
 } from './actions';
 
 // The initial state of the App
 const initialState = fromJS({
   modalStack: [],
+  progress: 0,
 });
 
 function appReducer(state = initialState, action) {
@@ -23,6 +25,9 @@ function appReducer(state = initialState, action) {
       newStack = newStack.pop();
       return state
         .set('modalStack', newStack);
+    }
+    case GLOBAL_PROGRESS: {
+      return state.set('progress', action.data);
     }
     default:
       return state;
