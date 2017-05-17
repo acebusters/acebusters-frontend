50ae67b485e52ec8913190c4d88233b55c6ee1ad
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-05-17 23:00:22 +0700

    Init test rig

--- app/containers/UiTestPage/index.js
+++ app/containers/UiTestPage/index.js
@@ -5,118 +5,48 @@ import React from 'react';
 import styled from 'styled-components';
 
 // components
+import Button from '../../components/Button';
 import ContainerBase from '../../components/Container';
-import H1 from '../../components/H1';
-import H2 from '../../components/H2';
-import H3 from '../../components/H3';
-import SeatComponent from '../../components/Seat2';
+import SeatExamples from './SeatExamples';
+import SeatTester from './SeatTester';
 
 const Container = styled(ContainerBase)`
   background-color: darkgray;
   background: radial-gradient(50% 49%, #B4B3B3 50%, #353535 100%);
 `;
 
-export default () => (
-  <Container>
-    <div>
-      <H1>UI Test Page</H1>
-      <H2>SeatComponent</H2>
-      <div style={{ display: 'flex' }}>
-        <div>
-          <H2>Transitions</H2>
-          <H3>Empty</H3>
-          <SeatComponent />
-          <H3>Paused</H3>
-          <SeatComponent
-            activePlayer={false}
-            chipCount="0"
-            username="Username123"
-            statusMsg={{
-              type: 'info',
-              text: 'sitting-in',
-              recent: true,
-            }}
-          />
+class UiTestPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTestPage: 'examples',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(val) {
+    this.setState({ currentTestPage: val });
+  }
+  render() {
+    const { currentTestPage } = this.state;
+    const showMain = () => {
+      if (currentTestPage === 'examples') return (<SeatExamples />);
+      if (currentTestPage === 'tester') return (<SeatTester />);
+      return (<SeatExamples />);
+    };
+    return (
+      <Container>
+        <div style={{ display: 'flex' }}>
+          <Button onClick={() => this.handleClick('examples')} value="examples">
+            Seat Examples
+          </Button>
+          <Button onClick={() => this.handleClick('tester')} value="tester">
+            Test Transitions
+          </Button>
         </div>
-        <div style={{ marginLeft: '4em' }}>
-          <H2>Recent Move</H2>
-          <h3>Call, Check</h3>
-          <SeatComponent
-            activePlayer
-            cards={['d2', 's2']}
-            chipCount="10,000"
-            username="123456789012"
-            statusMsg={{
-              type: 'success',
-              text: 'Call',
-              recent: true,
-            }}
-          />
-          <h3>Raise, Bet</h3>
-          <SeatComponent
-            activePlayer
-            cards={['d2', 's2']}
-            chipCount="10,000"
-            username="123456789012"
-            statusMsg={{
-              type: 'danger',
-              text: 'Raise',
-              recent: true,
-            }}
-          />
-          <h3>All-in, Winner</h3>
-          <SeatComponent
-            activePlayer
-            cards={['d2', 's2']}
-            chipCount="999"
-            username="Edith"
-            statusMsg={{
-              type: 'warning',
-              text: 'All-in',
-              recent: true,
-            }}
-          />
-        </div>
-        <div style={{ marginLeft: '4em' }}>
-          <H2>Old Move</H2>
-          <h3>Call, Check</h3>
-          <SeatComponent
-            activePlayer
-            cards={['d2', 's2']}
-            chipCount="10,000"
-            username="123456789012"
-            statusMsg={{
-              type: 'success',
-              text: 'Call',
-              recent: false,
-            }}
-          />
-          <h3>Raise, Bet</h3>
-          <SeatComponent
-            activePlayer
-            cards={['d2', 's2']}
-            chipCount="10,000"
-            username="123456789012"
-            statusMsg={{
-              type: 'danger',
-              text: 'Raise',
-              recent: false,
-            }}
-          />
-          <h3>All-in, Winner</h3>
-          <SeatComponent
-            activePlayer
-            cards={['d2', 's2']}
-            chipCount="999"
-            username="Edith"
-            statusMsg={{
-              type: 'warning',
-              text: 'All-in',
-              recent: false,
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  </Container>
-);
+        {showMain()}
+      </Container>
+    );
+  }
+}
+
+export default UiTestPage;
