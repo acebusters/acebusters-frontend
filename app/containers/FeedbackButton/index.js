7fabe7058ee42c35f41da5469da37f9b77ed7ff1
grabus <grabovalex@gmail.com>
2017-07-03 18:51:35 +0300

    add in progress state for feedback button (#308) (#358)

--- app/containers/FeedbackButton/index.js
+++ app/containers/FeedbackButton/index.js
@@ -8,11 +8,11 @@ import { conf } from '../../app.config';
 const Link = styled.a`
   cursor: pointer; 
   padding: 5px 10px;
-  background-color: cornflowerblue;
+  background-color: ${(props) => props.inProgress ? 'grey' : 'cornflowerblue'};
   transform: rotate(90deg);
   display: inline-block;
   position: fixed;
-  right: -36px;
+  right: ${(props) => props.inProgress ? '-44px' : '-36px'};
   bottom: 150px;
   color: white;
   border-radius: 0 0 3px 3px;
@@ -20,17 +20,32 @@ const Link = styled.a`
 
 export class FeedbackButton extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
+  constructor() {
+    super();
+    this.showReport = this.showReport.bind(this);
+    this.state = { inProgress: false };
+  }
+
   showReport() {
+    this.setState({ inProgress: true });
     Raven.captureMessage(`Feedback Button ${Date.now()}`);
     Raven.showReportDialog({
       eventId: Raven.lastEventId(),
       dsn: conf().sentryDSN,
     });
+    setTimeout(() => {
+      this.setState({ inProgress: false });
+    }, 5000);
   }
 
   render() {
+    const { inProgress } = this.state;
     return (
-      <Link onClick={this.showReport}>Report Crash</Link>
+      <Link onClick={!inProgress && this.showReport} inProgress={inProgress}>
+        {inProgress &&
+          <i className="fa fa-refresh fa-spin"></i>}
+        &nbsp;Report Crash
+      </Link>
     );
   }
 }
