a786e4111b62674fc65e8eba4750e036804ee13a
grabus <grabovalex@gmail.com>
2017-07-26 00:22:39 +0300

    change favicon (#445) (#449)

--- app/app.js
+++ app/app.js
@@ -37,7 +37,7 @@ import AccountProvider from 'containers/AccountProvider';
 
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-webpack-loader-syntax */
-import '!file-loader?name=[name].[ext]!./favicon.ico';
+import '!file-loader?name=[name].[ext]!./favicon.png';
 import '!file-loader?name=[name].[ext]!./manifest.json';
 import 'file-loader?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions
 /* eslint-enable import/no-webpack-loader-syntax */
