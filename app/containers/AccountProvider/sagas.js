0dd92a6d3c9cbcdf1b6a716c58d20e9bb4f622e4
Alex Lunyov <isuntc@gmail.com>
2017-07-26 05:21:56 +0800

    Use eth.getAccounts instead of eth.accounts for injected web3 (#447)

--- app/containers/AccountProvider/sagas.js
+++ app/containers/AccountProvider/sagas.js
@@ -14,6 +14,7 @@ import {
   ABI_ACCOUNT_FACTORY,
   ABI_PROXY,
 } from '../../app.config';
+import { promisifyContractCall } from '../../utils/promisifyContractCall';
 
 import { addEventsDate, getWeb3, isUserEvent } from './utils';
 
@@ -430,14 +431,18 @@ export function* ethEventListenerSaga(contract) {
 
 export function* injectedWeb3ListenerSaga() {
   while (true) { // eslint-disable-line no-constant-condition
-    yield call(delay, 1000);
-    const state = yield select();
-    const prevInjected = yield call([state, state.getIn], ['account', 'injected']);
-    const injected = window.web3 && window.web3.eth.accounts[0];
-
-    if (prevInjected !== injected) {
-      yield put(updateInjectedAccount(injected));
+    if (window.web3) {
+      const state = yield select();
+      const prevInjected = yield call([state, state.getIn], ['account', 'injected']);
+      const getInjectedAccounts = yield call(promisifyContractCall, window.web3.eth.getAccounts);
+      try {
+        const [injected] = yield call(getInjectedAccounts);
+        if (prevInjected !== injected) {
+          yield put(updateInjectedAccount(injected));
+        }
+      } catch (e) {} // eslint-disable-line no-empty
     }
+    yield call(delay, 5000);
   }
 }
 
