0a44d3f56eae9f25b080e8b4cff498ae0db45397
Alex Lunyov <isuntc@gmail.com>
2017-08-18 18:07:46 +0800

    Refresh all tables data in lobby. Preloader for refresh button (#536)

--- app/containers/Lobby/index.js
+++ app/containers/Lobby/index.js
@@ -3,36 +3,68 @@
  */
 
 import React from 'react';
-import { connect } from 'react-redux';
 import Button from 'components/Button';
 import Container from 'components/Container';
 import { TableStriped } from 'components/List';
 import H2 from 'components/H2';
 import { createStructuredSelector } from 'reselect';
 import LobbyItem from '../LobbyItem';
-import { tableReceived } from '../Table/actions';
+import { tableReceived, lineupReceived, updateReceived } from '../Table/actions';
 import { makeSelectLobby } from './selectors';
-import { fetchTables } from '../../services/tableService';
+import web3Connect from '../AccountProvider/web3Connect';
 import WithLoading from '../../components/WithLoading';
+import { fetchTableState, fetchTables } from '../../services/tableService';
 
+import { ABI_TABLE } from '../../app.config';
+
+async function getTableData(table, action) {
+  const [lineup, sb] = await Promise.all([
+    table.getLineup.callPromise(),
+    table.smallBlind.callPromise(),
+  ]);
+
+  action(table.address, lineup, sb);
+}
+
+const getTableHand = (tableAddr, action) => fetchTableState(tableAddr).then((rsp) => action(tableAddr, rsp));
 
 class LobbyComponent extends React.PureComponent { // eslint-disable-line
 
   constructor(props) {
     super(props);
-    this.handleGetTables = this.handleGetTables.bind(this);
-    this.handleGetTables();
+    this.handleRefresh = this.handleRefresh.bind(this);
+    this.web3 = props.web3Redux.web3;
+    this.state = {
+      refreshing: false,
+    };
   }
 
-  handleGetTables() {
-    fetchTables().then((tables) => {
+  componentDidMount() {
+    this.handleRefresh();
+  }
+
+  async handleRefresh() {
+    this.setState({ refreshing: true });
+    try {
+      const tables = await fetchTables();
       if (tables) {
-        tables.forEach((tableAddr) => this.props.tableReceived(tableAddr));
+        await Promise.all(tables.map((tableAddr) => {
+          const contract = this.web3.eth.contract(ABI_TABLE).at(tableAddr);
+          this.props.tableReceived(tableAddr);
+
+          return Promise.all([
+            getTableData(contract, this.props.lineupReceived),
+            getTableHand(tableAddr, this.props.updateReceived),
+          ]);
+        }));
       }
-    });
+    } finally {
+      this.setState({ refreshing: false });
+    }
   }
 
   render() {
+    const { refreshing } = this.state;
     const { lobby } = this.props;
 
     return (
@@ -62,15 +94,27 @@ class LobbyComponent extends React.PureComponent { // eslint-disable-line
           isLoading={lobby.length === 0}
         />
 
-        <Button onClick={this.handleGetTables} size="medium" icon="fa fa-refresh">REFRESH</Button>
+        <Button onClick={this.handleRefresh} size="medium">
+          Refresh
+          <WithLoading
+            isLoading={refreshing}
+            loadingSize="14px"
+            type="inline"
+            styles={{
+              inner: { marginLeft: 5 },
+            }}
+          />
+        </Button>
       </Container>
     );
   }
 }
 
-export function mapDispatchToProps(dispatch) {
+export function mapDispatchToProps() {
   return {
-    tableReceived: (tableAddr) => dispatch(tableReceived(tableAddr)),
+    tableReceived,
+    lineupReceived,
+    updateReceived,
   };
 }
 
@@ -80,7 +124,10 @@ const mapStateToProps = createStructuredSelector({
 
 LobbyComponent.propTypes = {
   lobby: React.PropTypes.array,
+  web3Redux: React.PropTypes.any,
   tableReceived: React.PropTypes.func,
+  updateReceived: React.PropTypes.func,
+  lineupReceived: React.PropTypes.func,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(LobbyComponent);
+export default web3Connect(mapStateToProps, mapDispatchToProps)(LobbyComponent);
--- app/containers/LobbyItem/index.js
+++ app/containers/LobbyItem/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { createStructuredSelector } from 'reselect';
 import Button from 'components/Button';
 import Link from 'components/Link';
 
-import web3Connect from '../AccountProvider/web3Connect';
-import { lineupReceived, updateReceived } from '../Table/actions';
-import { fetchTableState } from '../../services/tableService';
 import { makeSelectTableData, makeSelectTableLastHandId } from './selectors';
-import { ABI_TABLE } from '../../app.config';
 import { formatNtz } from '../../utils/amountFormatter';
 
 const Tr = styled.tr`
@@ -26,26 +23,7 @@ const Td = styled.td`
 
 const ADDR_EMPTY = '0x0000000000000000000000000000000000000000';
 
-const getTableData = (table, props) => {
-  const lineup = table.getLineup.callPromise();
-  const sb = table.smallBlind.callPromise();
-  return Promise.all([lineup, sb]).then((rsp) => {
-    props.lineupReceived(table.address, rsp[0], rsp[1]);
-    return Promise.resolve();
-  });
-};
-
-const getTableHand = (props) => fetchTableState(props.tableAddr).then((rsp) => props.updateReceived(props.tableAddr, rsp));
-
 class LobbyItem extends React.PureComponent { // eslint-disable-line
-
-  constructor(props) {
-    super(props);
-    this.web3 = props.web3Redux.web3;
-    this.table = this.web3.eth.contract(ABI_TABLE).at(props.tableAddr);
-    getTableData(this.table, props);
-    getTableHand(props);
-  }
 
   render() {
     if (!this.props.data || !this.props.data.seats) {
@@ -82,20 +60,12 @@ class LobbyItem extends React.PureComponent { // eslint-disable-line
 LobbyItem.propTypes = {
   tableAddr: React.PropTypes.string,
   data: React.PropTypes.object,
-  web3Redux: React.PropTypes.any,
   lastHandId: React.PropTypes.number,
 };
-
-export function mapDispatchToProps() {
-  return {
-    lineupReceived: (tableAddr, lineup, smallBlind) => (lineupReceived(tableAddr, lineup, smallBlind)),
-    updateReceived: (tableAddr, hand) => (updateReceived(tableAddr, hand)),
-  };
-}
 
 const mapStateToProps = createStructuredSelector({
   data: makeSelectTableData(),
   lastHandId: makeSelectTableLastHandId(),
 });
 
-export default web3Connect(mapStateToProps, mapDispatchToProps)(LobbyItem);
+export default connect(mapStateToProps)(LobbyItem);
