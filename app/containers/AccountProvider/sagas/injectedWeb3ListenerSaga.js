2fa63dfd83147a7262e626e994cb3f7a3a11e25c
Alex Lunyov <isuntc@gmail.com>
2017-08-18 18:12:23 +0800

    Fix metamask warning about provider (#537)

--- app/containers/AccountProvider/sagas/injectedWeb3ListenerSaga.js
+++ app/containers/AccountProvider/sagas/injectedWeb3ListenerSaga.js
@@ -1,5 +1,7 @@
 import { select, put, call } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
+import { getWeb3 } from '../../AccountProvider/utils';
+import { promisifyContractCall } from '../../../utils/promisifyContractCall';
 
 import { updateInjectedAccount } from '../actions';
 
@@ -9,11 +11,13 @@ export function* injectedWeb3ListenerSaga() {
       const state = yield select();
       const prevInjected = yield call([state, state.getIn], ['account', 'injected']);
       try {
-        const [injected] = window.web3.eth.accounts;
+        const [injected] = yield call(promisifyContractCall(getWeb3(true).eth.getAccounts));
         if (prevInjected !== injected) {
           yield put(updateInjectedAccount(injected));
         }
-      } catch (e) { } // eslint-disable-line no-empty
+      } catch (e) {
+        console.error(e);
+      } // eslint-disable-line no-empty
     }
 
     yield call(delay, 2000);
