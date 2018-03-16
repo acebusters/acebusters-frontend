c24d6f26fa3aee0d86b04a10181314cc01452f37
Alex Lunyov <isuntc@gmail.com>
2018-03-16 19:14:48 +0800

    Allow redux extension on production

--- app/store.js
+++ app/store.js
@@ -28,7 +28,6 @@ export default function configureStore(initialState = {}, history) {
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers =
-    process.env.NODE_ENV !== 'production' &&
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
